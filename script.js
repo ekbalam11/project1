0c8b62bac1fa00346905902fb4d1faa69f17e6f0
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2025-03-20 19:27:47 +0100

    feat: burguer nav bar

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
 function fetchData (){
     fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects')
   .then(response => response.json()) 
